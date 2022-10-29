2771cac1dbe5ce78f6d8106eec9e070d393bf1be
Gabriel Morato <87160136+gpurplem@users.noreply.github.com>
2022-10-29 15:20:20 -0300

    Página principal sem estar logado
    Página principal quando não há cookies de login. Primeiro acesso ao site, por exemplo.

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tic-Tac-Toe</title>
+
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+
+    <nav class="nav-top">
+        <div class="nav-container">
+            <ul class="nav-options">
+                <li><a href="">HOME</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <main class="main-index">
+        <div class="main-outer-container">
+            <div class="main-inner-container">
+                <div class="main-intro">
+                    <h2>Jogo da velha</h2>
+                    <blockquote cite="https://pt.wikipedia.org/wiki/Jogo_da_velha">
+                    A origem é desconhecida, com indicações de que pode ter começado no antigo Egito, onde foram encontrados tabuleiros esculpidos na rocha, que teriam mais de 3.500 anos.
+                    De alguma forma, é um jogo "aparentado" dos "Merels" (ver Marel).
+                    Algumas lendas urbanas contam que o jogo terá nascido em Portugal, na cidade de Almada no ano 545. No entanto, só foi popularizado no ano 1500, pelo descobridor Pedro Álvares Cabral, que adorava jogar este jogo durante as suas viagens.
+                    Álvares Cabral terá decidido que este jogo seria o primeiro a ser ensinado ao povo indígena no Brasil.
+                    O jogo pode ser jogado sobre um tabuleiro ou mesmo sendo riscado sobre um pedaço de papel ou mesa.
+                    O menor tabuleiro do mundo foi feito com DNA.
+                    </blockquote>
+                    <small><a href="https://pt.wikipedia.org/wiki/Jogo_da_velha">pt.wikipedia.org/wiki/Jogo_da_velha</a></small>
+                </div>
+                <div class="main-login">
+                    <form action="">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="">
+                        
+                        <label for="password">Senha</label>
+                        <input type="password" name="password" id="">
+
+                        <input type="button" value="LOGIN"><br>
+
+                        <p><a href="">Registre-se</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+</body>
+
+</html>
